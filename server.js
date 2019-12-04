131c9dcd0cd32c54ea1d44468b622e6f32ffbc40
Mark Halls <mark.halls@gmail.com>
2019-12-04 11:06:48 -0700

    auth commit

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
-const express = require('express'); // importing a CommonJS module
+const express = require("express"); // importing a CommonJS module
+const helmet = require(`helmet`);
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next();
+}
+
+function auth(req, res, next) {
+  const headers = req.headers;
+  if (headers.password === `mellon`) {
+    next();
+  } else {
+    res.status(401).json({ message: `you shall not pass` });
+  }
+}
+
+server.use(helmet());
 server.use(express.json());
+server.use(logger);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const nameInsert = req.name ? ` ${req.name}` : "";
 
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
+});
+
+server.get("/area51", auth, (req, res) => {
+  res.send(req.headers);
 });
 
 module.exports = server;
